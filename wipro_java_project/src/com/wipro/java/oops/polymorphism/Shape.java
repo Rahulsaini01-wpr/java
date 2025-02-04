2255deceeb10bd906d1db75b43b6e99c959c32a9
Rahulsaini01-wpr <rahul.sainy01@gmail.com>
2025-02-04 10:57:08 +0530

    Day 3
    Polymorphism Test

---
+++ wipro_java_project/src/com/wipro/java/oops/polymorphism/Shape.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.wipro.java.oops.polymorphism;
+
+/**
+ * The Parent class
+ */
+
+public class Shape {
+
+	 // method to render a shape
+	// This method will be overridden in child classes
+    public void draw() {
+        System.out.println("Drawing a Shape");
+    }
+	}
+
