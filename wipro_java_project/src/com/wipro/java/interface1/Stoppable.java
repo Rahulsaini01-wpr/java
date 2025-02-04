672a15cecb1c4aa3cebaddcd56d922887ac78d86
Rahulsaini01-wpr <rahul.sainy01@gmail.com>
2025-02-04 18:05:43 +0530

    Day 3
    Interface

---
+++ wipro_java_project/src/com/wipro/java/interface1/Stoppable.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.wipro.java.interface1;
+
+/**
+ * 
+ * This is the EngineStartStop interface
+ * An interface in Java is a contract that specifies what methods a class must implement.
+ */
+
+//Interface to represent the ability to stop something.
+public interface Stoppable {
+ void stop();  // Abstract method to stop.
+}
+
